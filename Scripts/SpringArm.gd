8c06a99c3fbe922bfd5cac611e79d3738452e364
Zheromaro <zahradin.dib@gmail.com>
2024-07-02 14:39:43 +0100

    PhantomCam & Qodot
    I tried these 2 Addon
    Qodot which work with trenchBroom for
    3d level design
    PhantomCam for dynamic camera movement

--- Scripts/SpringArm.gd
+++ Scripts/SpringArm.gd
@@ -14,4 +14,9 @@ func  _unhandled_input(event: InputEvent) -> void:
 		
 		rotation_degrees.y -= event.relative.x * mouse_sensitivity
 		rotation_degrees.y = wrapf(rotation_degrees.y, 0.0, 360.0)
-	
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("FreeMouse") and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	elif Input.is_action_just_pressed("FreeMouse") and Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
--- Scripts/mario.gd
+++ Scripts/mario.gd
@@ -14,6 +14,16 @@ extends CharacterBody3D
 @onready var jump_gravity := (-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)
 @onready var fall_gravity := (-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)
 
+@export_subgroup("camera")
+@export var min_pitch: float = -89.9
+@export var max_pitch: float = 50
+
+@export var min_yaw: float = 0
+@export var max_yaw: float = 360
+
+@export var mouse_sensitivity: float = 0.05
+@onready var _player_pcam: PhantomCamera3D = %PlayerPhantomCamera
+
 func _physics_process(delta: float) -> void:
 	var move_diraction := Vector3.ZERO
 	move_diraction.x = Input.get_action_strength("Left") - Input.get_action_strength("Right")
@@ -44,3 +54,27 @@ func get_gravity():
 
 func jump():
 	velocity.y = jump_velocity
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		
+		if _player_pcam.get_follow_mode() == _player_pcam.FollowMode.THIRD_PERSON:
+			var pcam_rotation_degrees : Vector3
+			
+			# Assigns the current 3D rotation of the SpringArm3D node - so it starts off where it is in the editor
+			pcam_rotation_degrees = _player_pcam.get_third_person_rotation_degrees()
+			
+			# Change the X rotation
+			pcam_rotation_degrees.x -= event.relative.y * mouse_sensitivity
+			
+			# Clamp the rotation in the X axis so it go over or under the target
+			pcam_rotation_degrees.x = clampf(pcam_rotation_degrees.x, min_pitch, max_pitch)
+			
+			# Change the Y rotation value
+			pcam_rotation_degrees.y -= event.relative.x * mouse_sensitivity
+			
+			# Sets the rotation to fully loop around its target, but witout going below or exceeding 0 and 360 degrees respectively
+			pcam_rotation_degrees.y = wrapf(pcam_rotation_degrees.y, min_yaw, max_yaw)
+			
+			# Change the SpringArm3D node's rotation and rotate around its target
+			_player_pcam.set_third_person_rotation_degrees(pcam_rotation_degrees)
